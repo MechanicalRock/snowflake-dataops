cf4c328350c2e9ec5f27cc1f459f50be6c43e950
zainab <maleki68.net@gmail.com>
2020-10-27 10:30:26 +0800

    updated init script to push to repo

--- pipeline/init.sh
+++ pipeline/init.sh
@@ -11,3 +11,15 @@ aws cloudformation wait stack-create-complete --stack-name ${STACK_NAME}
 # Print out all the CloudFormation outputs.
 aws cloudformation describe-stacks --stack-name ${STACK_NAME} --output table --query "Stacks[0].Outputs"
 
+export CODECOMMIT_REPO=`aws cloudformation describe-stacks --stack-name ${STACK_NAME} --output text --query "Stacks[0].Outputs[?OutputKey=='CodeCommitRepositoryCloneUrlHttp'].OutputValue"`
+
+echo "Initialising Git repository"
+git init
+echo "Adding the newly created CodeCommit repository as origin"
+git remote add origin ${CODECOMMIT_REPO}
+echo "Adding all files"
+git add .
+echo "CodeCommitting files"
+git commit -m "Initial commit"
+echo "Pushing to CodeCommit"
+git push --set-upstream origin master
